098e9b0a0e8eeab545511b5c3c3d4e4c0995417d
Gustavo Lins <gnlins@gmail.com>
2025-05-30 19:59:42 -0300

    fix: parameters order

--- todo.py
+++ todo.py
@@ -8,9 +8,10 @@ from dataclasses import dataclass
 class Todo:
     name: str
     due_date: date
+    user_id: int
     description: Optional[str] = None
     is_done: bool = False
-    user_id: int
+    
 
 if __name__ == '__main__':
     todo = Todo(name='ler um livro', due_date=date(2025, 5, 20), user_id=12)
