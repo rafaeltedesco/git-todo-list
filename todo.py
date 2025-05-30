15e2ad7f755ef2b9e360307ed49baaf647693e88
Rafael Tedesco <dev.rafaeltedesco@gmail.com>
2025-05-30 19:10:59 -0300

    feat: GitTodo-5575 - create todo dto

--- todo.py
+++ todo.py
@@ -1,1 +1,16 @@
-print("Hello Todo")
+from datetime import date
+from typing import Optional
+from dataclasses import dataclass
+
+# DTO - Data Transfer Object
+
+@dataclass
+class Todo:
+    name: str
+    due_date: date
+    description: Optional[str] = None
+    is_done: Optional[bool] = False
+    # user_id: int
+
+if __name__ == '__main__':
+    todo = Todo(name='ler um livro', due_date=date(2025, 5, 20))
